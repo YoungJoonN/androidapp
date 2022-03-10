410066a84c85f426e1a73be93441b394aec1a36a
YoungJoonNam <nyjoon2022@gmail.com>
2022-03-10 22:55:51 +0900

    android app project add

---
+++ Make20220228/app/src/main/java/com/example/make20220228/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.make20220228;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //안녕하세요
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
---
+++ Make20220228/app/src/main/java/com/example/make20220228/SubActivity.java
@@ -0,0 +1,15 @@
+package com.example.make20220228;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); //안녕하세요
+    }
+
+}
