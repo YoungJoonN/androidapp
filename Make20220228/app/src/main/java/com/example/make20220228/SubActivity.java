9fab01a9ef6576b2ea8a484ac2fe0aeef085ffb7
YoungJoonNam <nyjoon2022@gmail.com>
2022-03-16 22:48:49 +0900

    Andoriod App Study

--- Make20220228/app/src/main/java/com/example/make20220228/SubActivity.java
+++ Make20220228/app/src/main/java/com/example/make20220228/SubActivity.java
@@ -1,8 +1,10 @@
 package com.example.make20220228;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,17 @@ public class SubActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         // setContentView(R.layout.activity_main); //안녕하세요
         setContentView(R.layout.activity_sub); //안녕하세요
+
+        Button Btn2 = findViewById(R.id.Btn2);
+        Btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               Log.d("태그","값");
+            }
+            //33:29
+        });
+
+
     }
 
     public void Btn1Click(View v){
@@ -21,6 +34,6 @@ public class SubActivity extends AppCompatActivity
         Toast toast = Toast.makeText(this, "test", Toast.LENGTH_LONG);
         toast.setGravity(Gravity.END | Gravity.BOTTOM, 10,10);
         toast.show();
-        //31:34
+
     }
 }
